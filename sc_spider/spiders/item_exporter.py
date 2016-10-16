a0ee0bdbaf79fdd898779f680f9e09d747dd6a74
wings <wenqs27@gmail.com>
2016-10-17 00:17:31 +0800

    move exporter

--- sc_spider/spiders/item_exporter.py
+++
@@ -1,7 +0,0 @@
-from scrapy.contrib.exporter import JsonItemExporter
-
-
-class UnicodeJsonItemExporter(JsonItemExporter):
-    def __init__(self, file, **kwargs):
-        super().__init__(file, **kwargs)
-        self.encoder.ensure_ascii = False
