9c1e5c3ea911075c0aa14e265288b427cbb8480c
wings27 <wenqs27@gmail.com>
2017-01-08 11:59:14 +0800

    remove unused method.

--- sc_scrapy/spiders/spider_utils.py
+++ sc_scrapy/spiders/spider_utils.py
@@ -1,12 +1,4 @@
 import re
-from urllib.parse import urlparse
-
-
-def page_name_from_url(url, suffix='.txt'):
-    parsed = urlparse(url)
-    path = '.'.join(parsed.path.replace('/', '_').split('.')[0:-1])
-
-    return path + suffix
 
 
 def ignore_case_re(pattern_format):
